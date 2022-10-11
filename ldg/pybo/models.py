c15a94aac76d40b80580c48d3b959e0c50b96dbd
eoruadl <dg7202@gmail.com>
2022-10-11 17:47:43 +0900

    221011 커밋

---
+++ ldg/pybo/models.py
@@ -0,0 +1,7 @@
+from pybo import db
+
+class Question(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
