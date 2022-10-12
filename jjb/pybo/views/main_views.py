86bcb59f0c1a4beabcb73348118501fd7438b5cc
jae100 <108312143+jae100@users.noreply.github.com>
2022-10-13 00:06:03 +0900

    [EDU]점프 투 플라스크 학습
    2-8장까지 학습하였습니다~~

---
+++ jjb/pybo/views/main_views.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, url_for, render_template
+from werkzeug.utils import redirect
+
+from pybo.models import Question
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+@bp.route('/')
+def index():
+    return redirect(url_for('question._list'))
+
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id):
+    question = Question.query.get_or_404(question_id)
+    return render_template('question/question_detail.html', question=question)
