4fcac5876ea2b537dd6ef488472d5038a6f2bd17
eoruadl <dg7202@gmail.com>
2022-10-12 17:44:51 +0900

    [EDU] 플라스크 학습 2일차

--- ldg/pybo/__init__.py
+++ ldg/pybo/__init__.py
@@ -14,9 +14,12 @@ def create_app():
     # ORM
     db.init_app(app)
     migrate.init_app(app, db)
+    from . import models
 
     # 블루프린트
-    from .views import main_views
+    from .views import main_views, question_views, answer_views
     app.register_blueprint(main_views.bp)
+    app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
     
     return app
--- ldg/pybo/models.py
+++ ldg/pybo/models.py
@@ -5,3 +5,10 @@ class Question(db.Model):
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+
+class Answer(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
+    question = db.relationship('Question', backref=db.backref('answer_set'))
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
