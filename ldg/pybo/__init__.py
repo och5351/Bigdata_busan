60fd8ca031e1259f0e2015fe69ea5ae881a7c411
eoruadl <dg7202@gmail.com>
2022-10-13 17:49:38 +0900

    [EDU] 플라스크 학습 3일차

--- ldg/pybo/__init__.py
+++ ldg/pybo/__init__.py
@@ -17,9 +17,14 @@ def create_app():
     from . import models
 
     # 블루프린트
-    from .views import main_views, question_views, answer_views
+    from .views import main_views, question_views, answer_views, auth_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
+    app.register_blueprint(auth_views.bp)
+
+    # 필터
+    from .filter import format_datetime
+    app.jinja_env.filters['datetime'] = format_datetime
     
     return app
