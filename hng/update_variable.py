938964b8642b3f3b868f46d99fb0f336d1ed277c
Nagyeomhan <108378151+Nagyeomhan@users.noreply.github.com>
2022-11-07 19:47:32 +0900

    [EDU][BCK] SQL 스케줄링 기능 구현 및 관련 학습(수정: 11/07)

--- hng/update_variable.py
+++ hng/update_variable.py
@@ -50,19 +50,26 @@ engine.execute("SET GLOBAL event_scheduler = ON;")
 '''
 csv 파일 경로 세팅 => 추후 linux file system 경로로 수정
 '''
-# root_dir : 기본 경로
-root_dir = r'C:\Users\admin\Desktop\FinalProject\chromate\chromate_data\variable\\'
-# varfilelist : 기본 경로 안의 csv 파일 목록들
-varfilelist = natsort.natsorted(os.listdir(root_dir))
+# var_dir : 공정환경변수 파일 기본 경로
+var_dir = r'C:\Users\admin\Desktop\FinalProject\chromate\chromate_data\variable\\'
+# varfilelist : 공정환경변수 파일 기본 경로 안의 csv 파일 목록들
+varfilelist = natsort.natsorted(os.listdir(var_dir))
 varfilelist = [file for file in varfilelist if file.endswith('.csv')]
+
+# err_dir : 에러로트 파일 기본 경로
+err_dir = r'C:\Users\admin\Desktop\FinalProject\chromate\chromate_data\error\\'
+# errfilelist : 에러로트 파일 기본 경로 안의 csv 파일 목록들
+errfilelist = natsort.natsorted(os.listdir(err_dir))
+errfilelist = [file for file in errfilelist if file.endswith('.csv')]
+
 
 
 '''
 2. 전처리 함수 만들기
 '''
 # (1) csv 파일 하나 단위당 전처리하는 코드
-def PreProcessing(var_dir, var_list):
-    df = pd.read_csv(os.path.join(var_dir, var_list), engine='pyarrow')
+def PreProcessing(root_dir, var_list):
+    df = pd.read_csv(os.path.join(root_dir, var_list), engine='pyarrow')
 
     # (1)-1 csv 파일명으로부터 'Date' 컬럼 만들어주기
     df['Date'] = '-'.join(var_list.split('-')[-1].split('.')[:-1])
@@ -81,13 +88,12 @@ def PreProcessing(var_dir, var_list):
     
     return df
 
-
 # (2) csv 파일 전부 merge하고 정리하는 코드 
-def MergeFrame(var_dir, var_list):
+def MergeFrame(root_dir, var_list):
     # (2)-1 우선 row 하나로 데이터프레임 만들어놓고 거기에 merge해나간다
-    var_df = PreProcessing(var_dir, var_list[0])
+    var_df = PreProcessing(root_dir, var_list[0])
     for i in range(1, len(var_list)):
-        var_df = pd.merge(var_df, PreProcessing(var_dir, var_list[i]), how='outer')
+        var_df = pd.merge(var_df, PreProcessing(root_dir, var_list[i]), how='outer')
         
     # (2)-2 열 순서 조정 및 필요없는 'Index' 컬럼 삭제
     var_df = var_df[['Index', 'Date', 'Time', 'Lot', 'pH', 'Temp', 'Voltage']]
@@ -96,12 +102,30 @@ def MergeFrame(var_dir, var_list):
     return var_df
 
 
-variable_df = MergeFrame(root_dir, varfilelist)
-print(variable_df)
+# (3) csv 파일 하나 단위당 전처리하는 코드
+def ErrPreprocessing(root_dir, err_list):
+    df = pd.read_csv(os.path.join(root_dir, err_list), engine='pyarrow')
+    df.rename(columns = {'0':'Date', '1':'FailureLot1', '2':'FailureLot2'}, inplace=True)
+    
+    return df
+
+# (4) csv 파일 전부 merge하고 정리하는 코드
+def ErrMergeFrame(root_dir, err_list):
+    err_df = ErrPreprocessing(root_dir, err_list[0])
+    
+    # (4)-1 파일이 두 개 이상일 경우에만 merge 작업을 해주면 된다
+    if len(err_list) > 1:
+        for i in range(1, len(err_list)):
+            err_df = pd.merge(err_df, ErrPreprocessing(root_dir, err_list[i]), how='outer')
+            err_df = err_df[['Date', 'FailureLot1', 'FailureLot2']]
+    else:
+        err_df = err_df[['Date', 'FailureLot1', 'FailureLot2']]
+
+    return err_df
 
 
 
 '''
 3. Airflow 스케줄러 DAG 작성
-위의 과정을 스케줄로 할 수 있게 만든다
+위의 과정을 스케줄로 할 수 있게 만든다 - 계획: 하루 한 번
 '''
