014ff7cca4e7129991739690bb3c083f3479c9c9
eoruadl <dg7202@gmail.com>
2022-11-08 13:21:04 +0900

    [OPS] groovy 파일 test

---
+++ script.groovy
@@ -0,0 +1,32 @@
+// slack 채널 변수
+def SLACK_CHANNEL = ""
+
+// 함수
+def notifyCommon(slack_channel, message) {
+    slackSend (channel: "${slack_channel}", color: '#FFFF00', message: "${message}")
+}
+
+
+node('master') {
+    try {
+        //
+        stage('Start alert'){
+            notifyCommon(SLACK_CHANNEL, 'Clone Repository from git')
+        }
+
+        //
+        stage('Build'){
+
+        }
+        
+        stage('test'){
+
+        }
+
+        stage('deploy'){
+
+        }
+    }
+}
+
+//
