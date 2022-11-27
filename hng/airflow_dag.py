bc3e235501533ad7b616d3f8d5e87c7028cfd3d3
Nagyeomhan <108378151+Nagyeomhan@users.noreply.github.com>
2022-11-27 16:09:02 +0900

    [DEG][OPS] Airflow DAG 작성 (수정: 11/27)

--- hng/airflow_dag.py
+++ hng/airflow_dag.py
@@ -1,38 +1,48 @@
 '''
 1. 패키지 import
 '''
-# 기본 패키지 import
-import pandas as pd
-import numpy as np
-import pyarrow
-from datetime import datetime
+# # 기본 패키지 import
+# import pandas as pd
+# import numpy as np
+# import pyarrow
+# from datetime import datetime
 
-# 디렉토리 관련 패키지
-import os
-import glob
-import natsort
+# # 디렉토리 관련 패키지
+# import os
+# import glob
+# import natsort
 
-# MySQL 관련 패키지
-import MySQLdb
-import mysql.connector
+# # MySQL 관련 패키지
+# import MySQLdb
+# import mysql.connector
 
-# SQL Alchemy 관련 패키지 1
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.dialects.mysql import *
-from sqlalchemy.types import *
+# # SQL Alchemy 관련 패키지 1
+# import sqlalchemy
+# from sqlalchemy import create_engine
+# from sqlalchemy.dialects.mysql import *
+# from sqlalchemy.types import *
 
-# SQL Alchemy 관련 패키지 2
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Table, MetaData
-from sqlalchemy import insert, update
+# # SQL Alchemy 관련 패키지 2
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import Table, MetaData
+# from sqlalchemy import insert, update
 
-# Airflow 패키지
+# # Airflow 패키지
+# import airflow
+# import pendulum
+# from airflow import DAG
+# from airflow.operators.mysql_operator import MySQLOperator
+# from airflow.operators.python_operator import PythonOperator
+
+
+from datetime import datetime, timedelta
+from textwrap import dedent
 import airflow
 import pendulum
 from airflow import DAG
-from airflow.operators.mysql_operator import MySQLOperator
-from airflow.operators.python_operator import PythonOperator
+
+from airflow.operators.python import PythonOperator
+from airflow.providers.mysql.operators.mysql import MySqlOperator
 
 
 '''
@@ -40,14 +50,14 @@ from airflow.operators.python_operator import PythonOperator
 '''
 local_tz = pendulum.timezone('Asia/Seoul')
 init_args = {
-    'owner' : 'airflow',
-    'start_date' : datetime(2022, 11, 17, 0, tzinfo=local_tz),
-    'schedule_interval' : '0 0 * * *'
+    'owner' : 'airflow'
 }
 
 init_dag = DAG(
-    dag_id = 'sixdogma_chromate',
+    dag_id = 'sixdogma_variables',
     default_args = init_args,
+    start_date = datetime(2022, 11, 27, 15, tzinfo=local_tz),
+    schedule_interval = '@daily'
 )
 
 
@@ -56,13 +66,25 @@ init_dag = DAG(
 '''
 # 1. PreProcessing : CSV 파일 하나당 전처리하는 코드
 def PreProcessing(main_dir, var_dir):
-    # (1) 데이터프레임 생성하는 코드
+    # (1) 필요한 package import
+    # 기본적인 패키지
+    import pandas as pd
+    import numpy as np
+    import pyarrow
+    from datetime import datetime
+
+    # 디렉토리 관련 패키지
+    import os
+    import glob
+    import natsort
+
+    # (2) 데이터프레임 생성하는 코드
     df = pd.read_csv(os.path.join(main_dir, var_dir), engine='pyarrow')
 
-    # (2) csv 파일명으로부터 'Date' 컬럼 만들어주기
+    # (3) csv 파일명으로부터 'Date' 컬럼 만들어주기
     df['Date'] = '-'.join(var_dir.split('-')[-1].split('.')[:-1])
 
-    # (3) 'Time' 컬럼 전처리
+    # (4) 'Time' 컬럼 전처리
     adj_time = list()
     for time in df['Time']:
         tmp = time.split(':')
@@ -74,21 +96,33 @@ def PreProcessing(main_dir, var_dir):
         adj_time.append(tmp)
     df['Time'] = adj_time
 
-    # (4) 스케줄링을 위해서는 유니크 컬럼이 필요
+    # (5) 스케줄링을 위해서는 유니크 컬럼이 필요
     # 'Date'와 'Time' 합쳐서 'DateTime' 컬럼 만들기
     df['DateTime'] = df['Date'] + ' ' + df['Time']
     
-    # (5) 결과물 df 리턴
+    # (6) 결과물 df 리턴
     return df
 
 # 2. Merging : csv파일 전부 merge하고 정리하는 코드
 def Merging(file_dir):
-    # (1) 주어진 경로에서 파일들 불러내기
+    # (1) 필요한 package import
+    # 기본적인 패키지
+    import pandas as pd
+    import numpy as np
+    import pyarrow
+    from datetime import datetime
+
+    # 디렉토리 관련 패키지
+    import os
+    import glob
+    import natsort
+
+    # (2) 주어진 경로에서 파일들 불러내기
     varlist = natsort.natsorted(os.listdir(file_dir))
     varlist = [file for file in varlist if file.endswith('.csv')]
 
     # (2) 미리 빈 데이터프레임 생성하기
-    var_df = pd.DataFrame(columns=['Index', 'DateTime', 'Date', 'Time', 'Lot', 'pH', 'Temp', 'Voltage'])
+    var_df = pd.DataFrame()
 
     # (3) 전처리 코드 실행 : 모든 파일에 대해 반복문 돌리기
     for i in range(len(varlist)):
@@ -103,15 +137,29 @@ def Merging(file_dir):
     return var_df
 
 # 3. InputSQL : SQL Alchemy 실행시키고 집어넣는 코드
-def InputSQL(root_dir):
-    # (1) SQL Alchemy로 MySQL에 연결
+def InputSQL(root_dir, **kwargs):
+    # (1) 필요한 package import
+    # MySQL 관련 패키지
+    import MySQLdb
+    import mysql.connector
+
+    # SQL Alchemy 관련 패키지 1
+    import sqlalchemy
+    from sqlalchemy import create_engine
+
+    # SQL Alchemy 관련 패키지 2
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import Table, MetaData
+    from sqlalchemy import insert, update
+
+    # (2) SQL Alchemy로 MySQL에 연결
     url = 'mysql+mysqldb://sixdogma:Poiu0987*@13.113.12.130:3306/Test'
     engine = sqlalchemy.create_engine(url, encoding='utf-8', echo=True)
 
-    # (2) Merge한 데이터프레임 소환
+    # (3) Merge한 데이터프레임 소환
     var_df = Merging(root_dir)
 
-    # (3) to_sql()로 MySQL에 넣어주기
+    # (4) to_sql()로 MySQL에 넣어주기
     var_type = {'DateTime':sqlalchemy.types.DATETIME(),
                 'Date':sqlalchemy.types.DATE(),
                 'Time':sqlalchemy.types.TIME(),
@@ -134,8 +182,31 @@ def InputSQL(root_dir):
 '''
 5. 오퍼레이터 코드 작성
 '''
-df_to_sql = PythonOperator(
-    task_id= 'pd_to_sql',
+# dfprepro = PythonOperator(
+#     task_id = 'preprocessing',
+#     python_callable = PreProcessing,
+#     dag = init_dag
+# )
+
+# dfmerging = PythonOperator(
+#     task_id = 'merging',
+#     python_callable = Merging,
+#     dag = init_dag
+# )
+
+dftosql = PythonOperator(
+    task_id = 'inputsql',
     python_callable = InputSQL,
-    dag=init_dag
+    op_kwargs = {'root_dir': '/home/sixdogma/testinput/variable'},
+    dag = init_dag
 )
+
+
+'''
+6. DAG task 순서 설정
+'''
+# dfprepro >> dfmerging >> dftosql
+
+
+### 실제로 작동하는 함수는 InputSQL이고 나머지는 그 함수를 작동하기 위한 함수이므로
+### 실질적으로 InputSQL 하나만 돌리면 된다!
